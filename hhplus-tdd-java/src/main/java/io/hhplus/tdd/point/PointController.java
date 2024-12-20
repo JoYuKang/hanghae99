2731ed9b37226a9b02d814ab664a3447cd08f950
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2024-12-20 14:56:55 +0900

    # fix: service 명 변경

--- hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointController.java
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointController.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class PointController {
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
-    private final PointServiceImpl pointService;
+    private final PointService pointService;
 
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
